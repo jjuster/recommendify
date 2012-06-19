769b4d2c41cdd0653ff5356b420bdcf6f1792c80
Dan Morin <dan.morin@gmail.com>
2012-06-19 16:22:57 -0400

    Trying with old makefile

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -14,23 +14,23 @@ system("cd #{hiredis_dir} && make")
 # Statically link to hiredis (mkmf can't do this for us)
 $CFLAGS << " -I#{hiredis_dir}"
 $LDFLAGS << " #{hiredis_dir}/libhiredis.a"
-create_makefile('recommendity/ext')
+# create_makefile('recommendity/ext')
 
-# makefile = <<-MAKEFILE
-# all: prepare build
-# 
-# build:
-# 	gcc -Wall recommendify.c -lhiredis -o ../bin/recommendify
-# 
-# prepare:
-# 	mkdir -p ../bin
-# 
-# clean:
-# 	rm -f *.o
-# 
-# install: prepare
-# MAKEFILE
-#   
-# File.open(::File.expand_path("../Makefile", __FILE__), "w+") do |f|   
-#   f.write(makefile)
-# end
+makefile = <<-MAKEFILE
+all: prepare build
+
+build:
+	gcc -Wall recommendify.c -I#{hiredis_dir} -lhiredis -o ../bin/recommendify
+
+prepare:
+	mkdir -p ../bin
+
+clean:
+	rm -f *.o
+
+install: prepare
+MAKEFILE
+  
+File.open(::File.expand_path("../Makefile", __FILE__), "w+") do |f|   
+  f.write(makefile)
+end
